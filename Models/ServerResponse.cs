6cc4a3a75461761bf54e5093345ea71d61f331ed
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-01-04 22:34:26 -0300

    Classe ApplicationDbContext criada

--- Models/ServerResponse.cs
+++ Models/ServerResponse.cs
@@ -1,6 +1,10 @@
 ﻿namespace WebAPI_Funcionarios.Models
 {
-    public class ServerResponse
+    public class ServerResponse<T>
     {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Sucesso { get; set; } = true;
+
     }
 }
